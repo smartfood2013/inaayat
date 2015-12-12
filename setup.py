891bc5f46c67f4c05043b5a3379d2947f2bbf067
yomharaza <yomharaza@gmail.com>
2015-12-12 02:38:53 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='inaayat',
+    version='0.1',
+    packages=['inaayat', 'inaayat.test'],
+    url='http://techblog.younus-raza.com/',
+    license='MIT',
+    author='younus',
+    author_email='ymoharaza@gmail.com',
+    description='A Simple webserver'
+)
